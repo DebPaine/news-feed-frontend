c744abd4228b5f5d99bcde0a72df9300f72dd9c2
DebPaine <debmalyapaine@gmail.com>
2023-08-25 23:27:41 +0530

    add: fixed navbar

--- src/App.tsx
+++ src/App.tsx
@@ -19,22 +19,35 @@ function App() {
     }, []);
 
     return (
-        <div className='tile is-ancestor is-flex is-flex-wrap-wrap box'>
-            {headlines ?
-                headlines.map(({publishedAt, title, url, urlToImage, description}: any) =>
-                    <div className='tile is-parent'>
-                        <a href={url} target='_blank' className='tile is-child box notification is-info is-light'>
-                            <article>
-                                <figure className='image is-128x128'>
-                                    <img key={publishedAt} src={urlToImage} alt={title}/>
-                                </figure>
-                                <p className='is-size-7'>{description}</p>
-                            </article>
-                        </a>
-                    </div>) :
-                <p>Loading .....</p>}
-        </div>
-    )
+        <>
+            <nav className="navbar is-fixed-top is-dark" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <a className="navbar-item"
+                       target="_blank"
+                       href="https://static.toiimg.com/thumb/imgsize-37494,msid-101650517,width-400,resizemode-4/101650517.jpg">
+                        <img alt='News logo'
+                             src="https://static.toiimg.com/thumb/imgsize-37494,msid-101650517,width-400,resizemode-4/101650517.jpg"
+                             width="120" height="40"/>
+                    </a>
+                </div>
+            </nav>
+            <div className='tile is-ancestor is-flex is-flex-wrap-wrap box'>
+                {headlines ?
+                    headlines.map(({publishedAt, title, url, urlToImage, description}: any) =>
+                        <div className='tile is-parent'>
+                            <a href={url} target='_blank' className='tile is-child box notification is-dark'>
+                                <article>
+                                    <figure className='image is-128x128'>
+                                        <img key={publishedAt} src={urlToImage} alt={title}/>
+                                    </figure>
+                                    <p className='is-size-7'>{description}</p>
+                                </article>
+                            </a>
+                        </div>) :
+                    <p>Loading .....</p>
+                }
+            </div>
+        </>)
 }
 
 export default App
